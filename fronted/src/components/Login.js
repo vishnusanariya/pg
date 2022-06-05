287d3a675f8416905a16962d9ea58343ff5bc171
vishnusanariya <vishnusanariya98@gmail.com>
2022-06-05 10:48:20 +0530

    login page

--- fronted/src/components/Login.js
+++ fronted/src/components/Login.js
@@ -1,10 +1,46 @@
 import React from 'react'
-
+import './Login.css'
 function Login() {
   return (
-    <div>
-      <h3>Login</h3>
+    <>
+   <section className="vh-100 h-75">
+  <div className="container py-5 h-75 w-75 mt-5">
+    <div className="row d-flex align-items-center justify-content-center h-100">
+      <div className="col-md-8 col-lg-7 col-xl-6 h-auto">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+          className="img-fluid" alt="Phone"/>
+      </div>
+      <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1 h-auto w-auto">
+        <form >
+          {/* <!-- Email input --> */}
+          <div className="form-outline mb-4">
+            <input type="email" id="form1Example13" className="form-control form-control-lg" />
+            <label className="form-label" for="form1Example13">Email address</label>
+          </div>
+
+          {/* <!-- Password input --> */}
+          <div className="form-outline mb-4">
+            <input type="password" id="form1Example23" className="form-control form-control-lg" />
+            <label className="form-label" for="form1Example23">Password</label>
+          </div>
+
+          <div className="d-flex justify-content-between align-items-center ml-0">
+            {/* <!-- Checkbox --> */}
+            <div className="form-check ">
+              <input className="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+              <label className="form-check-label" for="form1Example3"> Remember me </label>
+            </div>
+            <a href="#!">Forgot password?</a>
+          </div>
+
+          {/* <!-- Submit button --> */}
+          <button type="submit" className="btn btn-primary btn-lg btn-block">Sign in</button>
+        </form>
+      </div>
     </div>
+  </div>
+</section>
+   </>
   )
 }
 
--- fronted/src/components/Register.js
+++ fronted/src/components/Register.js
@@ -2,67 +2,66 @@ import React from 'react'
 import './Register.css'
 function Register() {
   return (
-  <>
-  <div className="container">
-  <form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Name</label>
-      <input type="text" class="form-control" id="inputEmail4" placeholder="Name"/>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">confirm Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
-    </div>
-    <div class="form-group col-md-6">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-  </div>
-  <div class="form-group col-md-6">
-    <label for="inputAddress2">pincode</label>
-    <input type="number" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-  </div>
-  <div class="form-group col-md-6">
-    <label for="inputAddress2">contact</label>
-    <input type="number" class="form-control" id="contact" placeholder="+91"/>
-  </div>
-  <div class="form-group col-md-6">
-    <label for="inputAddress2">rent</label>
-    <input type="number" class="form-control" id="rent" placeholder="INR"/>
-  </div>
-  <div class="form-group col-md-6">
-    <label for="inputAddress2">person</label>
-    <input type="number" class="form-control" id="person" placeholder="e.g.3"/>
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">accomodation_facilities</label>
-      <input type="text" class="form-control" id="inputCity" placeholder='wifi,AC,TV,furniture..'/>
-    </div>
-  </div>
-  </div>
-  
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        lodging
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
-  </div>
-  </>
-    )
+    <>
+      <div className="container mt-5">
+        <form className='form-control'>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputEmail4"><i class="zmdi zmdi-account-circle"></i> Name</label>
+              <input type="text" class="form-control" id="inputEmail4" placeholder="Name" />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputEmail4"><i class="zmdi zmdi-email"></i> Email</label>
+              <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4"><i class="zmdi zmdi-lock"></i> Password</label>
+              <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4"><i class="zmdi zmdi-lock-outline"></i> confirm Password</label>
+              <input type="password" class="form-control" id="inputPassword4" placeholder="confirm Password" />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputAddress"><i class="zmdi zmdi-pin"></i> Address</label>
+              <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputAddress2"><i class="zmdi zmdi-pin-drop"></i> pincode</label>
+              <input type="number" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputAddress2"><i class="zmdi zmdi-phone"></i> contact</label>
+              <input type="number" class="form-control" id="contact" placeholder="+91" />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputAddress2"><i class="zmdi zmdi-money-box"></i> rent</label>
+              <input type="number" class="form-control" id="rent" placeholder="INR" />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputAddress2"><i class="zmdi zmdi-accounts-alt"></i> person</label>
+              <input type="number" class="form-control" id="person" placeholder="e.g.3" />
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputCity"><i class="zmdi zmdi-comments"></i> accomodation_facilities</label>
+                <input type="text" class="form-control" id="inputCity" placeholder='wifi,AC,TV,furniture..' />
+            </div>
+          </div>
+          <div className="form-raw">
+            <div class="form-group col-md-6">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck" />
+              <label class="form-check-label" for="gridCheck">
+              <i class="zmdi zmdi-cutlery"></i> lodging
+              </label>
+            </div>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary">Sign in</button>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default Register
